de3bbe99b9b00842c22a0ebb16ada61e0d706c7c
Karim <karimshalapy@gmail.com>
2020-06-28 21:40:28 +0200

    Setup event listeners

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 //CALCULATION MODULE
 var Calculation = (function () {
+    //Profession Constructor
+    //Merchant EXP for each level Calc
+
 
 })();
 //UI CONTROL MODULE
@@ -21,15 +24,56 @@ var UIController = (function () {
         workerResources: "worker-resources",
         workerBtn: "worker-calc",
         workerAns: "worker-answer",
+        mainPanel: "main"
     };
     //Public functions
     return {
         getDOMStrings: function () {
-            return DOMstrings
+            return DOMStrings
+        },
+        getInput: function (type) {
+            return {
+                lvl: parseFloat(document.getElementById(DOMStrings[type + "Lvl"]).value),
+                exp: parseFloat(document.getElementById(DOMStrings[type + "Exp"]).value),
+                resources: parseFloat(document.getElementById(DOMStrings[type + "Resources"]).value),
+            };
         },
     }
 })()
 //GLOBAL APP CONTROLER MODULE
-var Controller = (function (Calcs, UICtrl) {
-    var DOMStrings = UICtrl.getDOMStrings()
+var controller = (function (Calcs, UICtrl) {
+    var DOMStrings, input;
+    DOMStrings = UICtrl.getDOMStrings();
+    var setupEventListener = function () {
+        document.getElementById(DOMStrings.mainPanel).addEventListener("click", function (event) {
+            var clicked, input, type;
+            clicked = event.target.id;
+            type = clicked.split("-")[0];
+            if (clicked === DOMStrings.merchantBtn) {
+                input = UICtrl.getInput(type);
+                console.log(input);
+            } else if (clicked === DOMStrings.workerBtn) {
+                input = UICtrl.getInput(type);
+                console.log(input);
+            } else if (clicked === DOMStrings.crafterBtn) {
+                input = UICtrl.getInput(type);
+                console.log(input);
+            };
+        })
+    }
+    //1. get input
+
+    //2. calc values
+    //3. print Ans to UI
+
+    return {
+        test: function () {
+        },
+        init: function () {
+            console.log("App started...")
+            setupEventListener();
+        }
+    }
+
 })(Calculation, UIController);
+controller.init();
