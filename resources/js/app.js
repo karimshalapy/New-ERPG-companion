aaf5e8588ef896018cef7f441a568269f8022d01
Karim <karimshalapy@gmail.com>
2020-07-01 01:48:28 +0200

    Late commit Added Apple Calculator and Apple calculator function

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,10 @@ var Calculation = (function () {
     var merchantAns = new Answers(0, 0, 0);
     var workerAns = new Answers(0, 0, 0);
     var crafterAns = new Answers(0, 0, 0);
+    var resourcesAns = {
+        logs: 0,
+        apples: 0,
+    }
 
     //Old Merchant Eq
     /*var merchantEq = function (i) {
@@ -129,11 +133,26 @@ var Calculation = (function () {
             worker: workerExpReqForLvl,
             crafter: crafterExpReqForLvl,
         },
+        resourcesCalc: function (type, resources) {
+            if (type === "a3") {
+                resourcesAns.log = resources * 2 * 3.75 * 1.5 * 1.5
+            } else if (type === "a5") {
+                resourcesAns.log = resources * 4 * 3.75 * 1.5 * 1.5
+            } else if (type === "a7") {
+                resourcesAns.log = resources * 1.5 * 1.5
+            } else if (type === "a8") {
+                resourcesAns.log = resources * 8 * 1.5 * 1.5
+            } else if (type === "a9") {
+                resourcesAns.log = resources * 2 * 1.5
+            };
+            resourcesAns.apples = resourcesAns.log / 8
+        },
         getAnswers: function () {
             return {
                 merchant: merchantAns,
                 worker: workerAns,
                 crafter: crafterAns,
+                resources: resourcesAns,
             };
         },
     };
@@ -176,6 +195,12 @@ var UIController = (function (calcs) {
         crafterChoice: "crafter-start",
         workerChoice: "worker-start",
         swiperNext: "swiper-button-next",
+        resourcesLvl: "resources-type",
+        resourcesExp: "resources-type",
+        resourcesResources: "resources-resources",
+        resourcesOldResources: "resources__resources-old-input",
+        resourcesBtn: "resources-calc",
+        resourcesAns: "resources-answer",
         type: ["welcome", "merchant", "crafter", "worker"],
     };
     //Disable scroll
@@ -375,3 +400,86 @@ var mySwiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     },
 });
+/*********************************************/
+/***Dropdown menu code************************/
+
+var x, i, j, l, ll, selElmnt, a, b, c;
+/* Look for any elements with the class "custom-select": */
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+    selElmnt = x[i].getElementsByTagName("select")[0];
+    ll = selElmnt.length;
+    /* For each element, create a new DIV that will act as the selected item: */
+    a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    /* For each element, create a new DIV that will contain the option list: */
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items select-hide");
+    for (j = 1; j < ll; j++) {
+        /* For each option in the original select element,
+        create a new DIV that will act as an option item: */
+        c = document.createElement("DIV");
+        c.innerHTML = selElmnt.options[j].innerHTML;
+        c.addEventListener("click", function (e) {
+            /* When an item is clicked, update the original select box,
+            and the selected item: */
+            var y, i, k, s, h, sl, yl;
+            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+            sl = s.length;
+            h = this.parentNode.previousSibling;
+            for (i = 0; i < sl; i++) {
+                if (s.options[i].innerHTML == this.innerHTML) {
+                    s.selectedIndex = i;
+                    h.innerHTML = this.innerHTML;
+                    y = this.parentNode.getElementsByClassName("same-as-selected");
+                    yl = y.length;
+                    for (k = 0; k < yl; k++) {
+                        y[k].removeAttribute("class");
+                    }
+                    this.setAttribute("class", "same-as-selected");
+                    break;
+                }
+            }
+            h.click();
+        });
+        b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener("click", function (e) {
+        /* When the select box is clicked, close any other select boxes,
+        and open/close the current select box: */
+        e.stopPropagation();
+        closeAllSelect(this);
+        this.nextSibling.classList.toggle("select-hide");
+        this.classList.toggle("select-arrow-active");
+    });
+}
+
+function closeAllSelect(elmnt) {
+    /* A function that will close all select boxes in the document,
+    except the current select box: */
+    var x, y, i, xl, yl, arrNo = [];
+    x = document.getElementsByClassName("select-items");
+    y = document.getElementsByClassName("select-selected");
+    xl = x.length;
+    yl = y.length;
+    for (i = 0; i < yl; i++) {
+        if (elmnt == y[i]) {
+            arrNo.push(i)
+        } else {
+            y[i].classList.remove("select-arrow-active");
+        }
+    }
+    for (i = 0; i < xl; i++) {
+        if (arrNo.indexOf(i)) {
+            x[i].classList.add("select-hide");
+        }
+    }
+}
+
+/* If the user clicks anywhere outside the select box,
+then close all select boxes: */
+document.addEventListener("click", closeAllSelect);
